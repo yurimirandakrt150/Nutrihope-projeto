51047c55fbec8ce679615685671e2c4d002f113f
Welsete <wellerson197@hotmail.com>
2024-04-23 03:00:31 -0300

    Adicionado JS a todas as paginas para o botão

--- script/comofunciona.js
+++ script/comofunciona.js
@@ -44,3 +44,12 @@ button.addEventListener('click', function(){
         return button.textContent = "Ver Mais";
     }
 })
+document.querySelector('.botao-header').addEventListener('click', function() {
+    // Alterna a visibilidade do menu de navegação ao clicar no botão
+    var menu = document.querySelector('.menu-navegacao');
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+ });
--- script/home.js
+++ script/home.js
@@ -37,3 +37,13 @@ novaDiv.appendChild(imagem);
 
 // usei para inserir a div antes do footer
 footer_div.parentNode.insertBefore(novaDiv, footer_div);
+
+document.querySelector('.botao-header').addEventListener('click', function() {
+    // Alterna a visibilidade do menu de navegação ao clicar no botão
+    var menu = document.querySelector('.menu-navegacao');
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+ });
